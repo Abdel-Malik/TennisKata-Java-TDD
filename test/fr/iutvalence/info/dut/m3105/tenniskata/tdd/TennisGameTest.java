bde2140f93549bb5a0c1d44d991e1458325dad17
blouin maxime <maxime.blouin@iut-valence.fr>
2015-10-20 12:48:46 +0200

    Commit 1 getScore - test score nul

--- test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
+++ test/fr/iutvalence/info/dut/m3105/tenniskata/tdd/TennisGameTest.java
@@ -1,15 +1,18 @@
 package fr.iutvalence.info.dut.m3105.tenniskata.tdd;
 
 
+import static org.junit.Assert.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TennisGameTest
 {
 	@Test
-	public void aSimpleBeginningTest()
-	{
-		Assert.fail("...");
+	public void aTestWhenTheScoreIsNullForTheTwoPlayers(){
+		TennisGame currentGame = new TennisGame();
+		assertEquals(currentGame.getScore(),"love-all");
+
 	}
 
 }
